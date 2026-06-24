d5904b81eebb12c568d1ad4ff13eeba17280714d
Michael Dales <mwd24@cam.ac.uk>
2026-06-24 12:21:55 +0100

    Make deno build work on clean install

--- frontend/build.ts
+++ frontend/build.ts
@@ -3,14 +3,31 @@ import { denoPlugins } from "jsr:@luca/esbuild-deno-loader";
 
 await Deno.mkdir("dist", { recursive: true });
 
-// Resolve laz-perf's local cache path via deno info
-const cmd = new Deno.Command("deno", {
+// We need to copy the WASM blob for laz-perf, and given we're not using
+// deno as the server it seems we have to copy that manually still. We do
+// this by finding where deno keeps libraries and extracting it ourselves.
+const infoCmd = new Deno.Command("deno", {
+    args: ["info", "--json"],
+    stdout: "piped",
+});
+const infoRes = await infoCmd.output();
+const info = JSON.parse(new TextDecoder().decode(infoRes.stdout));
+
+// We need to get the verion number also for whatever we're using
+const pkgInfoCmd = new Deno.Command("deno", {
   args: ["info", "--json", "npm:laz-perf"],
   stdout: "piped",
 });
-const output = await cmd.output();
-const info = JSON.parse(new TextDecoder().decode(output.stdout));
-const lazPerfPath = info.npmPackages["laz-perf@0.0.7"].localPath;
+const pkgInfoRes = await pkgInfoCmd.output();
+const pkg = JSON.parse(new TextDecoder().decode(pkgInfoRes.stdout));
+
+const lazPerfKey = Object.keys(pkg.npmPackages).find(k => k.startsWith("laz-perf@"));
+if (!lazPerfKey) {
+    throw new Error("laz-perf not found");
+}
+const lazPerfVersion = lazPerfKey.split("@")[1];
+
+const lazPerfPath = `${info.npmCache}/registry.npmjs.org/laz-perf/${lazPerfVersion}`;
 
 await esbuild.build({
   plugins: [...denoPlugins()],
@@ -19,7 +36,7 @@ await esbuild.build({
   bundle: true,
   platform: "browser",
   format: "esm",
-  minify: false,
+  minify: true,
   treeShaking: true,
 });
 
